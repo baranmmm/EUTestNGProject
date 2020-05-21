0972bc783337663da9a5a70e026eaa57f8d86a5d
baranmmm <m.barann84@gmail.com>
2020-05-21 04:01:12 +0100

    updated

--- src/test/java/com/cybertek/tests/TestBase.java
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -1,9 +1,13 @@
 package com.cybertek.tests;
 
+import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import com.cybertek.utilities.Sleeper;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -15,6 +19,9 @@ public class TestBase {
     public WebDriver driver;
     public Actions actions;
     public WebDriverWait wait;
+    public Faker faker;
+
+
 
     @BeforeMethod
     public void setUp() {
@@ -23,6 +30,12 @@ public class TestBase {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         actions=new Actions(driver);
         wait=new WebDriverWait(driver, 10);
+        faker=new Faker();
+        driver.get(ConfigurationReader.get("url"));
+
+
+
+
 
     }
 
